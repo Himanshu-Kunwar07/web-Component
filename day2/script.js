6f57b70ee307823a6d2ebff4302d1f51cf60ba06
Himanshu Kunwar <himanshukunwar07@gmail.com>
2024-08-22 01:38:10 +0530

    day-02

---
+++ day2/script.js
@@ -0,0 +1,13 @@
+
+
+ function setImage(pic){
+    
+    const div = document.querySelector(".preview");
+    const child = document.querySelector("#previewimage");
+    const image = document.createElement('img');
+    image.src = pic.src;
+    image.setAttribute('id', 'previewimage');
+    div.removeChild(child);
+    div.appendChild(image);
+    
+}
